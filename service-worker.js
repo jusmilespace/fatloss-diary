9f528043a65366cbabcfb1d9445801f2719f26eb
Ju Smile <ju.smilespace@gmail.com>
2025-10-31 11:28:16 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+/* Ju Smile PWA Service Worker */
+const CACHE_NAME = 'jusmile-pwa-v3';
+const PRECACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json',
+  './privacy.html',
+  './404.html',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(PRECACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(
+      keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e)=>{
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(
+    caches.match(req).then(cached=>{
+      const fetchPromise = fetch(req).then(res=>{
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(c=>c.put(req, copy));
+        return res;
+      }).catch(()=>cached);
+      return cached || fetchPromise;
+    })
+  );
+});
